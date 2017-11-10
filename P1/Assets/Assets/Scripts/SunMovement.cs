796583ac206f05afeb1c4b747bf82b120372f7b1
lucasdemaatdeltion <97045549@st.deltion.nl>
2017-11-10 11:39:34 +0100

    push

---
+++ P1/Assets/Assets/Scripts/SunMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour {
+
+    public float sunmovementSpeed = 60f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xAngle = Time.fixedDeltaTime * sunmovementSpeed / 360;
+        transform.Rotate(xAngle,0,0);
+		
+	}
+}
